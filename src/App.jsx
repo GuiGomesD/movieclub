40f0c1ca07a363cc9d1af2eef502f70e7c00d405
Rômulo Henri <123867521+romhenri@users.noreply.github.com>
2023-06-11 11:27:41 -0300

    import: React Router Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import Header from './components/Header'
+import Home from './Home'
+import Default from './Default'
+import Error404 from './Error404'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './css/tailwind.css'
 import './css/App.css'
 
 function App() {
 	return (
-		<>
+		<BrowserRouter>
 			<Header />
-		</>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/me" element={<Home />} />
+				<Route path="/about" element={<Home />} />
+				<Route path="/default" element={<Default />} />
+				<Route path="*" element={<Error404 />} />
+			</Routes>
+		</BrowserRouter>
 	)
 }
 
