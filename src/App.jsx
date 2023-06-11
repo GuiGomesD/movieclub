88b7eae7ecd56dc74354ba0ddc14fe605cd7021a
Rômulo Henri <123867521+romhenri@users.noreply.github.com>
2023-06-11 11:11:40 -0300

    build: Inicial structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import Header from './components/Header'
 import './css/tailwind.css'
 import './css/App.css'
 
-const tailwindLogo =
-	'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg'
-
 function App() {
-	const [count, setCount] = useState(0)
-
 	return (
 		<>
-			<div className="flex justify-between">
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
-					/>
-				</a>
-				<a href="https://tailwindcss.com/" target="_blank">
-					<img
-						src={tailwindLogo}
-						className="tailwind logo"
-						alt="Tailwind logo"
-					/>
-				</a>
-			</div>
-			<h1>Vite + React + Tailwind</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p className="mt-6">
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className=" read-the-docs">
-				Click on the Vite, React and Tailwind logos to learn more
-			</p>
+			<Header />
 		</>
 	)
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import '../css/Header.css'
+
+const Header = () => {
+	return <header className="header">MovieClub</header>
+}
+
+export default Header
