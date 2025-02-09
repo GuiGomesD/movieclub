371cae311420370d26094c0b683d6d62bf9e21a8
Guilherme Gomes <134429767+GuiGomesD@users.noreply.github.com>
2025-02-09 00:04:04 -0300

    feat: Initial Layout with Integrated API

---
+++ client/src/components/rodape.vue
@@ -0,0 +1,16 @@
+<template>
+    <section class="footer">
+        <p>Footer</p>
+    </section>
+</template>
+
+<style scoped>
+    .footer {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #111111;
+        padding: 150px 0px;
+        color: #F1F1F1;
+    }
+</style>
